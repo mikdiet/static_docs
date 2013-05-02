3232fac0a03fc1bca7f281a6bb739ce074c50e6e
Mikhail Dieterle <MikDiet@gmail.com>
2013-05-02 16:57:05 +0300

    use single action both for root page and inner pages

--- app/controllers/static_docs/pages_controller.rb
+++ app/controllers/static_docs/pages_controller.rb
@@ -2,11 +2,6 @@ require_dependency "static_docs/application_controller"
 
 module StaticDocs
   class PagesController < ApplicationController
-    def index
-      @page = Page.matched("home")
-      render :show
-    end
-
     def show
       @page = Page.matched(params[:page_path])
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 StaticDocs::Engine.routes.draw do
-  root :to => 'pages#index'
+  root :to => 'pages#show', :page_path => 'home'
   get '*page_path' => 'pages#show'
 
 
