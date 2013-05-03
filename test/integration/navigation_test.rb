5926131412d2eabd6d6eb66b9623b5f4130c4ee8
Mikhail Dieterle <MikDiet@gmail.com>
2013-05-04 00:23:56 +0300

    render body in view

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -7,15 +7,20 @@ class NavigationTest < ActionDispatch::IntegrationTest
     get '/'
     assert_response :success
     assert_select 'title', {:text => 'Dummy'}, 'Page must be rendered within application layout'
-    assert_select 'body', /This is home page/
+    assert_select 'h3', {:text => 'This is home page'}
 
     get '/page'
     assert_response :success
+    assert_select 'h3', {:text => 'This is first level page'}
     assert_select 'body', /This is first level page/
 
     get '/namespace/page'
     assert_response :success
-    assert_select 'body', /This is namespaced page/
+    assert_select 'h3', {:text => 'This is namespaced page'}
+
+    get '/markdown'
+    assert_response :success
+    assert_select 'pre', {:text => '### This is markdown page'}
 
     get '/wrong'
     assert_response :not_found
